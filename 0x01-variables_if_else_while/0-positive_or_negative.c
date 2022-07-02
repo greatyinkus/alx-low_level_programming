cb62a1d55b3407ab167eb996f2915b120e3d2536
greatyinkus <106778196+greatyinkus@users.noreply.github.com>
2022-07-02 02:47:50 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 /**
-*main - Print the alphabet in lowercase, except for q ans e.
-*
-*Return: Always 0.
-*/
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-  char letter;
-  
-  for (letter = a 'a'; letter <+ 'z'; letter ++)
-  {
-    if (letter != 'e' && letter != 'q')
-      putcha(letter);
-  }
-  
-  putchar('\n');
-  
-  return (0);
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
 }
