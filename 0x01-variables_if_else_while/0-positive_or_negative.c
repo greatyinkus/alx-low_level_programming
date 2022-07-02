bb31f15b9b3e26ee9fa331a177afe0276a6eb025
greatyinkus <106778196+greatyinkus@users.noreply.github.com>
2022-07-02 02:46:59 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,27 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 /**
-* main - Entry point
-* Return: Always 0 (Success)
+*main - Print the alphabet in lowercase, except for q ans e.
+*
+*Return: Always 0.
 */
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there */
-if (n > 0)
-{
-printf("%d is positive\n", n);
-}
-if (n < 0)
-{
-printf("%d is negative\n", n);
-}
-if (n == 0)
-{
-printf("%d is zero\n", n);
-}
-return (0);
+  char letter;
+  
+  for (letter = a 'a'; letter <+ 'z'; letter ++)
+  {
+    if (letter != 'e' && letter != 'q')
+      putcha(letter);
+  }
+  
+  putchar('\n');
+  
+  return (0);
 }
