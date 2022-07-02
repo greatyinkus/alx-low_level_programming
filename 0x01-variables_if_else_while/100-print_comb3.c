3b0dff9459194c26c0f0ecc1b8d5422a7d85e22d
greatyinkus <106778196+greatyinkus@users.noreply.github.com>
2022-07-02 01:20:53 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
 
 /**
- * main - prints the numbers from 00 to 99
- * Return: Always 0 (success)
+ * main - prints all possible different combinations of three digits
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n, m;
+	int n, m, l;
 
-	for (n = 48; n <= 57; n++)
+	for (n = 48; n < 58; n++)
 	{
-		for (m = 48; m <= 57; m++)
+		for (m = 49; m < 58; m++)
 		{
-			putchar(n);
-			putchar(m);
-			if (n != 57 || m != 57)
+			for (l = 50; l < 58; l++)
 			{
-				putchar(',');
-				putchar(' ');
+				if (l > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
 			}
 		}
 	}
