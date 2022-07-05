c7c9461fcdfe6cc6bb3b3fcc30f88485d3801bc9
greatyinkus <adeyinka.aiyegbusi@yahoo.com>
2022-07-05 15:37:57 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,4 @@
+# 0x02-functions_nested_lops
+
+## 0. _putchar
+Program that prints "putchar", followed by a new line.
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+alx-low_level_program
+This task is for low low level programming
