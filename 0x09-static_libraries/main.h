fe0131d41aebac1de41a92d403ed577fca284f16
greatyinkus <106778196+greatyinkus@users.noreply.github.com>
2022-07-23 17:37:15 +0100

    Update main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_H
+
 #define MAIN_H
+
 
 
 int _putchar(char c);
@@ -41,6 +43,7 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *haystack, char *needle);
+
 
 
 #endif
